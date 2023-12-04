8019873ea27ee6bdb2d902af895fdf64bbb5ea92
phudyka <paul.hudyka@gmail.com>
2023-12-04 15:18:36 +0100

    main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:27:37 by phudyka           #+#    #+#             */
-/*   Updated: 2023/11/07 16:30:25 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/11/28 17:02:34 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 int main( void ) {
 Fixed a;
-Fixed b( a );
 Fixed c;
+
+Fixed b( a );
+
+std::cout << "Fixed 'b' raw value " << b.getRawBits() << std::endl;
+
+b.setRawBits(42);
+
 c = b;
+
 std::cout << a.getRawBits() << std::endl;
 std::cout << b.getRawBits() << std::endl;
 std::cout << c.getRawBits() << std::endl;
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,26 +6,30 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:27:37 by phudyka           #+#    #+#             */
-/*   Updated: 2023/11/10 10:32:28 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/12/04 15:14:53 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int main( void ) {
-	
-	Fixed			a;
-	Fixed const		b( Fixed( 5.05f ) * Fixed( 2 ) );
-	
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-
 	std::cout << b << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
 
-	std::cout << Fixed::max( a, b ) << std::endl;
-
+	std::cout << "\nTESTS:\n";
+	Fixed c = a;
+	std::cout << Fixed::min(c, b) << std::endl;
+	std::cout << b / Fixed(2) << std::endl;
+	std::cout << b + Fixed(-3.14f) << std::endl;
+	std::cout << b - Fixed(18) << std::endl;
+	std::cout << (c != a) << std::endl;
+	std::cout << (c == a) << std::endl;
 	return 0;
 }
